e58c69a59bebfb4155a8c0415ff0dfb262865d82
Allen Visser <128151824+AllenOnAzure@users.noreply.github.com>
2025-08-21 14:51:56 +0200

    Add files via upload

---
+++ tags/Require-tag-on-resource-groups/RGtag.tf
@@ -0,0 +1,69 @@
+#Require a tag and its value on resource groups
+
+DEFINITION:
+Require a tag and its value on resource groups
+ASSIGNMENT:
+Require a tag and its value on resource groups
+
+################## PROVIDER ########################################
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
+
+################################## RESOURCES ######################
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+  
+##################   POLICY DEFINITION TAG   ##################
+
+resource "azurerm_policy_definition" "require_environment_tag" {
+  name         = "Resource-Groups-require-environment-tag"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Require environment tag on resource groups"
+  description  = "Enforces resource groups with 'environment' tag with allowed values."
+
+  parameters = jsonencode({
+    allowedValues = {
+      type = "Array"
+      metadata = {
+        description = "Allowed values for the environment tag"
+      }
+      defaultValue = ["dev", "test", "prod"]
+    }
+  })
+
+  policy_rule = jsonencode({
+    if = {
+      allOf = [
+        {
+          field  = "type"
+          equals = "Microsoft.Resources/subscriptions/resourceGroups"
+        },
+        {
+          not = {
+            field = "tags['environment']"
+            in    = "[parameters('allowedValues')]"
+          }
+        }
+      ]
+    }
+    then = {
+      effect = "deny"
+    }
+  })
+}
