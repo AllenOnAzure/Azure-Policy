a0e032b731831b659deffc018421056bbb7c372a
Allen Visser <128151824+AllenOnAzure@users.noreply.github.com>
2025-08-27 09:48:05 +0200

    Add files via upload

---
+++ tags/Enforces-a-required-tag-and-its-value-on-resource-groups/RGtag.tf
@@ -0,0 +1,80 @@
+################## PROVIDER ############################################################
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
+
+################################## RESOURCES ##########################################
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+##################   POLICY DEFINITION TAG   ###########################################
+
+resource "azurerm_policy_definition" "require_environment_tag" {
+  name         = "Enforces a required tag and its value on resource groups"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Enforces a required tag and its value on resource groups"
+  description  = "Enforces resource groups with 'environment' tag with allowed values"
+  metadata = jsonencode({
+    version  = "1.0.0"
+    category = "Tags"
+  })
+
+  parameters = jsonencode({
+    allowedValues = {
+      type = "Array"
+      metadata = {
+        description = "Allowed values for the environment tag"
+      }
+      defaultValue = ["dev", "test", "prod"]
+    }
+  })
+
+  policy_rule = jsonencode({
+    if = {
+      allOf = [
+        {
+          field  = "type"
+          equals = "Microsoft.Resources/subscriptions/resourceGroups"
+        },
+        {
+          not = {
+            field = "tags['environment']"
+            in    = "[parameters('allowedValues')]"
+          }
+        }
+      ]
+    }
+    then = {
+      effect = "deny"
+    }
+  })
+}
+#################### POLICY ASSIGNMENT ########################################
+
+resource "azurerm_subscription_policy_assignment" "assignment" {
+  name                 = "Enforces a required tag and its value on resource groups"
+  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/8ce3da23-7156-49e4-b145-24f95f9dcb46"
+  subscription_id      = "/subscriptions/${var.subscription_id}"
+
+  parameters = jsonencode({
+    effect = {
+      value = "Audit"
+    }
+  })
+}
+################################################################################
