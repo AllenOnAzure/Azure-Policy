36f00c634f902d196d0b967056b12b8c11efd342
Allen Visser <128151824+AllenOnAzure@users.noreply.github.com>
2025-09-03 20:22:50 +0200

    Update rgtagv3.tf
    cleanup

--- tags/Enforces-a-required-tag-and-its-value-on-resource-groups/rgtagv3.tf
+++ tags/Enforces-a-required-tag-and-its-value-on-resource-groups/rgtagv3.tf
@@ -232,35 +232,27 @@ resource "azurerm_subscription_policy_assignment" "assignment" {
   subscription_id      = "/subscriptions/${var.subscription_id}"
 
   parameters = jsonencode({
-
     platformAllowedValues = {
       value = ["Finance", "IT", "Sales", "Marketing"]
     }
-
     costcenterAllowedValues = {
       value = ["9800", "5566", "3012", "4512"]
     }
-
     BCAllowedValues = {
       value = ["Tier 0 - Mission-Critical", "Tier 1 - Business-Critical", "Tier 2 - Business-Operational", "Tier 3 - Dev/Test/Low Priority"]
     }
-
     replicationAllowedValues = {
       value = ["Geo-redundant storage (GRS)", "Zone-redundant storage (ZRS)", "Locally redundant storage (LRS)"]
     }
-
     snoozingAllowedValues = {
       value = ["None", "BusinessHours-8x5", "Off Hours-Weekends"]
     }
-
     environmentAllowedValues = {
       value = ["Development", "Test", "Production", "Sandbox", "Staging", "UAT"]
     }
-
     roleAllowedValues = {
       value = ["Allen Visser", "Bruce Wayne", "Clark Kent", "Barry Allen", "Dick Grayson", "Jason Todd"]
     }
-
     createdByAllowedValues = {
       value = ["Allen Visser", "Bruce Wayne", "Clark Kent", "Barry Allen", "Dick Grayson", "Jason Todd"]
     }
@@ -280,4 +272,5 @@ resource "azurerm_subscription_policy_assignment" "assignment" {
       value = ["External_Vendor1", "External_Vendor2", "External_Vendor3"]
     }
   })
+
 }      
